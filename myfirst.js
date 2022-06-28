e852e5f08f124a8878aa7c6f6e706de711123590
yeapes <yeapesh2831@gmail.com>
2022-06-28 13:15:47 +0600

    print request url

--- myfirst.js
+++ myfirst.js
@@ -5,7 +5,8 @@ var dt = require('./myfirstModule');
 //create a server object
 http.createServer(function (req, res) { 
     res.writeHead(200, {'Content-Type': 'text/html'}); 
-    res.write("The date and time are currently :" + dt.myDateTime()); 
+    res.write("The date and time are currently :" + dt.myDateTime());
+    res.write(req.url);
     res.end(); //end the response
 }).listen(8080); //the server object listens on port 8080
 
