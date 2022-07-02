f2a8a575bc58563bf0f75bd9d1246558b4029c17
yeapes <yeapesh2831@gmail.com>
2022-07-02 12:24:19 +0600

    fs

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const path = require('path');
 const os = require('os');
-const myPath = 'C:/laragon/www/node-basic/index.js';
-const outputPath = path.extname(myPath);
-console.log(outputPath);
+const fs = require('fs');
+// const myPath = 'C:/laragon/www/node-basic/index.js';
+// const outputPath = path.extname(myPath);
+// console.log(outputPath);
 
 
-console.log(os.cpus());
+// console.log(os.cpus());
+
+fs.writeFileSync('hello.txt','Hello World!');
+fs.appendFileSync('hello.txt','I\'m from bangladesh');
