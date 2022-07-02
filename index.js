b6f1268e2c0055d7b452cac26bbc64d6a26a32c1
yeapes <yeapesh2831@gmail.com>
2022-07-02 12:12:54 +0600

    os

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const path = require('path');
-
+const os = require('os');
 const myPath = 'C:/laragon/www/node-basic/index.js';
-const outputPath = path.basename(myPath);
+const outputPath = path.extname(myPath);
 console.log(outputPath);
+
+
+console.log(os.cpus());
